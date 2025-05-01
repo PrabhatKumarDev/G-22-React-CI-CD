04794fa42835333827f11f1bb1e9306cec56e41f
Prabhat Kumar <prabhat.kumar.hisar@gmail.com>
2025-05-01 20:32:38 +0530

    Adding files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>My React App</h1>
+        <h2>This is a simple React app implemented with CI/CD Pipeline</h2>
+        <h3>With Github Actions</h3>
+      </div>
+    </>
+  );
+}
+
+export default App;
